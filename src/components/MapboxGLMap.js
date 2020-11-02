dbaefcb0c579786b508adb6bff14dd88bf0ee579
Jonatan Engstad <jonaengs@gmail.com>
2020-11-02 18:46:51 +0100

    try to add gyroscope and accelerometer

--- src/components/MapboxGLMap.js
+++ src/components/MapboxGLMap.js
@@ -8,9 +8,28 @@ const styles = {
   position: "absolute"
 };
 
+let gyro = new Gyroscope({frequency: 60});
+let accl = new Accelerometer({frequency: 60});
+gyro.start();
+accl.start();
+accl.addEventListener('reading', () => {
+  console.log("Acceleration along the X-axis " + accl.x);
+  console.log("Acceleration along the Y-axis " + accl.y);
+  console.log("Acceleration along the Z-axis " + accl.z);
+});
+gyro.addEventListener('reading', e => {
+  console.log("Angular velocity along the X-axis " + gyro.x);
+  console.log("Angular velocity along the Y-axis " + gyro.y);
+  console.log("Angular velocity along the Z-axis " + gyro.z);
+});
+
+
 const MapboxGLMap = () => {
   const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
+  
+  const layerTypes = ["light", "dark", "outdoors", "satellite"];
+
 
   useEffect(() => {
     mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
@@ -18,7 +37,7 @@ const MapboxGLMap = () => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-        center: [10.408773,63.422091],
+        center: [10.408773, 63.422091],
         zoom: 10
       });
 
